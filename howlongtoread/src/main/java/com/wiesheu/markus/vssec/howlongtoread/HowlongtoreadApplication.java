71ac89a98f9564a8eaed3e988af286c3c55b9778
Markus Wiesheu <markus.wiesheu@hm.edu>
2020-10-22 12:56:40 +0200

    initialized spring project

---
+++ howlongtoread/src/main/java/com/wiesheu/markus/vssec/howlongtoread/HowlongtoreadApplication.java
@@ -0,0 +1,13 @@
+package com.wiesheu.markus.vssec.howlongtoread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowlongtoreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowlongtoreadApplication.class, args);
+	}
+
+}
---
+++ howlongtoread/src/test/java/com/wiesheu/markus/vssec/howlongtoread/HowlongtoreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiesheu.markus.vssec.howlongtoread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowlongtoreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
